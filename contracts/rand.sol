6b9ab760b17d95579f7ab14f93918f01e6a3c3f7
YutaSugimura <nextc3m@gmail.com>
2018-11-06 01:15:43 +0900

    rand contracts

--- contracts/rand.sol
+++ contracts/rand.sol
@@ -4,9 +4,9 @@ contract Rand {
 
   uint256 private randSeed = 1;
 
-  mapping(address => uint256) internal randToAddress;
+  mapping(address => uint256[]) internal randToAddress;
 
-  function _rand() internal view returns(uint256) {
+  function _rand() private view returns(uint256) {
     uint256 rand = uint256(sha3(now, block.number, randSeed));
 
     return rand %= (10 ** 6);
@@ -14,6 +14,10 @@ contract Rand {
 
   function _updateSeed() internal {
     randSeed = _rand();
+  }
+
+  function viewRand() public view returns(uint256) {
+    return randToAddress[msg.sender];
   }
 
 }
